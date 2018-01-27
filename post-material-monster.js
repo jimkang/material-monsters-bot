8bb669637a967865ab8a26234161315121d36999
Jim Kang <jimkang@gmail.com>
2018-01-26 20:39:16 -0500

    Post to archive.

--- post-material-monster.js
+++ post-material-monster.js
@@ -5,6 +5,24 @@ var createMonsterNamer = require('material-monsters');
 var seedrandom = require('seedrandom');
 var Twit = require('twit');
 
+var StaticWebArchiveOnGit = require('static-web-archive-on-git');
+var queue = require('d3-queue').queue;
+var randomId = require('idmaker').randomId;
+
+var staticWebStream = StaticWebArchiveOnGit({
+  config: config.github,
+  title: config.archiveName,
+  footerScript: `<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49491163-1', 'jimkang.com');
+  ga('send', 'pageview');
+</script>`,
+  maxEntriesPerPage: 50
+});
+
 var twit = new Twit(config.twitter);
 
 var dryRun = false;
@@ -19,34 +37,43 @@ var namer = createMonsterNamer({
   random: seedrandom(seed)
 });
 
-namer.nameMonster(postName);
+namer.nameMonster(postToTargets);
 
-function postName(error, namePackage) {
+function postToTargets(error, namePackage) {
   if (error) {
-    console.log(error);
+    logError(error);
   } else {
-    console.log(namePackage);
-    postTweet(namePackage.name);
+    var text = namePackage.name;
+    if (dryRun) {
+      console.log('Would have tweeted:', text);
+    } else {
+      var q = queue();
+      q.defer(postTweet, text);
+      q.defer(postToArchive, text);
+      q.await(logError);
+    }
   }
 }
 
-function postTweet(text) {
-  if (dryRun) {
-    console.log('Would have tweeted:', text);
-  } else {
-    twit.post(
-      'statuses/update',
-      {
-        status: text
-      },
-      function tweetDone(error, data) {
-        if (error) {
-          console.log(error);
-          console.log('data:', data);
-        } else {
-          console.log('Posted to Twitter:', text);
-        }
-      }
-    );
+function postTweet(text, done) {
+  var body = {
+    status: text
+  };
+  twit.post('statuses/update', body, done);
+}
+
+function postToArchive(text, done) {
+  var id = 'monster-' + randomId(8);
+  staticWebStream.write({
+    id,
+    date: new Date().toISOString(),
+    caption: text
+  });
+  staticWebStream.end(done);
+}
+
+function logError(error) {
+  if (error) {
+    console.log(error);
   }
 }
